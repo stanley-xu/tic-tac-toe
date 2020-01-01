b2b15f3b0e10419b08e3f1bc4ca94d1594a0e672
Stanley Xu <16456356+stanley-xu@users.noreply.github.com>
2020-01-01 12:43:01 -0500

    Fix: jumping in either chrono order

--- src/historysidebar.js
+++ src/historysidebar.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import Board from './board';
 
+// The sidebar handles jumping and rendering the histories
+//  in the proper order
+// - The jump function passed via props works only with the original indicies orders
 export default function HistorySidebar(props) {
   const history = props.reversed ?
     props.history.reverse() : props.history;
-  const jumpFn = props.onClick;
+
   return (
     <ul className='sidebar'>
       {
         history.map((prevState, moveNo) => {
+          const jumpIdx = props.reversed ?
+            history.length - moveNo - 1 : moveNo;
+
           return (
             <li key={moveNo}>
               <Board
                 previewStyle='preview'
                 squares={prevState.squares}
-                onClick={() => jumpFn(history.length - moveNo - 1)}/>
+                onClick={() => props.onClick(jumpIdx)}/>
             </li>
           );
         })
