59d55d184d188c8c886d662561f87e2568c3c6e4
Stanley Xu <16456356+stanley-xu@users.noreply.github.com>
2020-01-10 21:12:22 -0500

    Updates

---
+++ precache-manifest.5b4842dc0be7b0ec7d4f81d7d37d424d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4f8856957bbd2266af5ec1e30e1be3",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "72ad81cbf01bda378416",
+    "url": "/tic-tac-toe/static/css/main.198c7f49.chunk.css"
+  },
+  {
+    "revision": "9d288fc88ccd0b1e6b06",
+    "url": "/tic-tac-toe/static/js/2.76875ba0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe/static/js/2.76875ba0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72ad81cbf01bda378416",
+    "url": "/tic-tac-toe/static/js/main.6a210f2b.chunk.js"
+  },
+  {
+    "revision": "14c00c4b9a734bdeb7a0",
+    "url": "/tic-tac-toe/static/js/runtime-main.1fec3c3c.js"
+  }
+]);
